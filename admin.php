7705c43f9edecbb189a297b8805689fb1d42272e
kirzin <kirzin06@gmail.com>
2023-02-04 23:50:53 +0800

    #change

--- admin.php
+++ admin.php
@@ -27,7 +27,7 @@ $sqlProject = mysqli_query($con,$queryProject);
 <head>
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
+<!-- samp -->
   <title>Dashboard - Alegario Cure Hospital</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
